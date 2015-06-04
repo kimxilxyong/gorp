e1478d44cd7409fc5271fc405156ed2c5b3fa01a
Kim Ilyong <kimxilxyong@gmail.com>
2015-06-04 07:03:43 +0200

    added DSN info to test_all.bat

--- test_all.bat
+++ test_all.bat
@@ -6,12 +6,14 @@ echo http://github.com/kimxilxyong/gorp
 set GORP_TEST_DSN=gorptest:gorptest@/gorptest?parseTime=true
 set GORP_TEST_DIALECT=gomysql
 echo Running tests for %GORP_TEST_DIALECT%
+echo Database DSN: %GORP_TEST_DSN%
 
 go test .
 
 set GORP_TEST_DSN=user=gorptest password=gorptest dbname=gorptest sslmode=disable
 set GORP_TEST_DIALECT=postgres
 echo Running tests for %GORP_TEST_DIALECT%
+echo Database DSN: %GORP_TEST_DSN%
 
 go test .
 
